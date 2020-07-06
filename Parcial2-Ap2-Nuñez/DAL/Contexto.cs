4d7f77cb9fd72110c20eac9070ee2b97d62ae8a6
Steven112 <54567861+Steven112@users.noreply.github.com>
2020-07-05 20:15:47 -0400

    Add project files.

---
+++ Parcial2-Ap2-Nuñez/DAL/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_Ap2_Nuñez.DAL
+{
+    public class Contexto : DbContext
+    {
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= C:\Users\stive\OneDrive\Escritorio\BD\APO.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+
+
+    }
+}
